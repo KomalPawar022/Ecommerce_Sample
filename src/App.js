814bddd501df859868320103671958134e89e2ad
Komal <139200853+KomalPawar022@users.noreply.github.com>
2024-01-22 13:05:18 +0000

    Parent - Child Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./styles.css";
 import Home from "./component/Home.js";
 import ConditionalComponent from "./component/ConditionalComponent";
+import ParentComponent from "./component/ParentComponent";
 
 export default function App() {
   return (
     <div className="App">
       <ConditionalComponent />
+      <ParentComponent />
+
       <Home />
     </div>
   );
