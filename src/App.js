891d7c26b483be6ccf909aec73c9f381e8872c92
Komal <139200853+KomalPawar022@users.noreply.github.com>
2024-01-23 07:34:58 +0000

    Product added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./component/Home";
 import img from "./assets/bg.jpg";
-import StateFunctionComponent from "./component/StateFunctionComponent";
+import Product from "./component/Product";
 
 function App() {
   return (
     <>
-      <StateFunctionComponent />
       <Home image={img} />
+      <Product />
     </>
   );
 }
