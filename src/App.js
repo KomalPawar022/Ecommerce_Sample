2b485a878b235480e5a417bc1ae8dbced21f28b2
Komal <139200853+KomalPawar022@users.noreply.github.com>
2024-01-23 19:19:33 +0000

    Order

--- src/App.js
+++ src/App.js
@@ -7,17 +7,25 @@ import About from "./component/About";
 import Contact from "./component/Contact";
 import Product from "./component/Product";
 
+import { OrdersComponent } from "./component/orders/OrdersComponent";
+import { OrderDetails } from "./component/orders/OrderDetails";
+import { DefaultChild } from "./component/orders/DefaultChild";
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<Home image={img} />} />
-          <Route path="about" element={<About />}>
-            <Route path="contact" element={<Contact />} />
-            <Route path="product" element={<Product />} />
-          </Route>
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="product" element={<Product />} />
           <Route path="*" element={<div>Page Not Found</div>} />
+
+          <Route path="Orders" element={<OrdersComponent />}>
+            <Route index element={<DefaultChild />} />
+            <Route path=":orderid" element={<OrderDetails />} />
+          </Route>
         </Routes>
       </div>
     </Router>
