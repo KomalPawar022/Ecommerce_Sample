f498ec2789a34d1be98c49aad453636ca91d0c96
Komal <139200853+KomalPawar022@users.noreply.github.com>
2024-01-23 07:04:29 +0000

    StateFunctionComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./component/Home";
 import img from "./assets/bg.jpg";
-import StateComponent from "./component/StateComponent";
+import StateFunctionComponent from "./component/StateFunctionComponent";
 
 function App() {
   return (
     <>
-      <StateComponent />
+      <StateFunctionComponent />
       <Home image={img} />
     </>
   );
