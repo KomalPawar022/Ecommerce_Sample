439320e1e5c775956d7e1bf7eb58d74b6e61a331
Komal <139200853+KomalPawar022@users.noreply.github.com>
2024-01-23 15:31:14 +0000

    Routing

---
+++ src/component/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+function About() {
+  return (
+    <div>
+      <h1>This is About Page</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo blanditiis,
+      sint quis eligendi magnam aperiam facilis id cupiditate quia deserunt sunt
+      provident, commodi doloremque deleniti a aliquam facere amet. Dolorem.
+      Iure laborum quod minus temporibus enim magnam, suscipit sit molestiae
+      magni illum iste neque excepturi voluptatibus perferendis veniam minima
+      quidem dolorem? Ratione eaque magnam veniam officia sunt, totam iusto.
+      Nemo.
+    </div>
+  );
+}
+export default About;
